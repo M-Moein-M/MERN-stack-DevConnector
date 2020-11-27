177bec3d69d9b66c734cd9659e3a111c97907fe0
Moein <mohammadmoein.moradi@gmail.com>
2020-11-27 22:16:14 +0330

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('SERVER IS ON FIRE!!');
+});
