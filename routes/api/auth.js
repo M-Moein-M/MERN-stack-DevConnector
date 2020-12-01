5e2708ea07abd8935e4c4870353fd1f3b16a4b95
Moein <mohammadmoein.moradi@gmail.com>
2020-12-01 21:47:19 +0330

    feat: add retrieving user from a protected route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,17 @@
 const router = require('express').Router();
+const auth = require('../../middleware/auth');
+const User = require('./../../models/User');
 
 // @route   GET api/auth
 
-router.get('/', (req, res) => {
-  res.send('Auth route');
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json({ user });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
 });
 
 module.exports = router;
