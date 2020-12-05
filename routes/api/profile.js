acf29c7f277695d4f56c16c1e339583af6504721
Moein <mohammadmoein.moradi@gmail.com>
2020-12-05 07:55:13 +0330

    feat: add get profile for logged in user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,28 @@
 const router = require('express').Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const user = require('../../models/User');
 
-// @route   GET api/profile
+// @route   GET api/profile/me
+// @desc get current user's profile
+// @access private
 
-router.get('/', (req, res) => {
-  res.send('Profile route');
+router.get('/me', auth, async (req, res) => {
+  try {
+    // get the profile from database
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There's no profile for this user" });
+    }
+
+    profile.populated('user', ['name', 'avatar']);
+
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
 });
 
 module.exports = router;
