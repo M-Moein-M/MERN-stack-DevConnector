60eab66a828c16b1e31ecf1fc8110fd4be2e719a
Moein <mohammadmoein.moradi@gmail.com>
2020-12-14 09:08:11 +0330

    feat: add route for adding new post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,37 @@
 const router = require('express').Router();
+const { request } = require('express');
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-// @route   GET api/posts
+// @route   POST api/post
+// @desc    create a post
+// @access  private
+router.post('/', [auth, [check('text', 'Text is required').not().isEmpty()]], async (req, res) => {
+  const errors = validationResult(req);
+  // check for errors
+  if (!errors.isEmpty()) {
+    return res.status(400).send({ errors: errors.array() });
+  }
 
-router.get('/', (req, res) => {
-  res.send('Posts route');
+  try {
+    // get user
+    const user = await User.findById(req.user.id).select('-password');
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id,
+    });
+
+    const post = await newPost.save();
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
 });
-
 module.exports = router;
