ac535d1d8466e5b2613d952a839609e7781ac8e4
Moein <mohammadmoein.moradi@gmail.com>
2020-12-23 21:01:42 +0330

    chore: setup router for login & register

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,21 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 const App = () => {
   return (
     <Router>
       <Fragment>
-        <Navbar></Navbar>
-        <Route exact path='/' component={Landing}></Route>
-        <Landing></Landing>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
       </Fragment>
     </Router>
   );
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,23 +1,24 @@
 import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <Fragment>
       <nav className='navbar bg-dark'>
         <h1>
-          <a href='index.html'>
+          <Link to='/'>
             <i className='fas fa-code'></i> DevConnector
-          </a>
+          </Link>
         </h1>
         <ul>
           <li>
-            <a href='profiles.html'>Developers</a>
+            <a href='!#'>Developers</a>
           </li>
           <li>
-            <a href='register.html'>Register</a>
+            <Link to='/register'>Register</Link>
           </li>
           <li>
-            <a href='login.html'>Login</a>
+            <Link to='/login'>Login</Link>
           </li>
         </ul>
       </nav>
