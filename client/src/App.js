0c29993bbbd80a7994482777add1de891062d55a
Moein <mohammadmoein.moradi@gmail.com>
2020-12-23 20:43:25 +0330

    chore: setup react router & landing & navbar comp.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 
 const App = () => {
   return (
-    <Fragment>
-      <Navbar></Navbar>
-      <Landing></Landing>
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar></Navbar>
+        <Route exact path='/' component={Landing}></Route>
+        <Landing></Landing>
+      </Fragment>
+    </Router>
   );
 };
 
