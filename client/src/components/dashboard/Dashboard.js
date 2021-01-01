7819389c2d87fc64acc273bf8391741e275e3327
Moein <mohammadmoein.moradi@gmail.com>
2021-01-01 18:01:46 +0330

    chore: fetch profile on dashboard load

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,8 +1,24 @@
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile';
+import React, { useEffect } from 'react';
 
-const Dashboard = (props) => {
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, []);
   return <div>DASHBOARD</div>;
 };
 
-Dashboard.prototype = {};
-export default Dashboard;
+Dashboard.prototype = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  profile: state.profile,
+});
+
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
