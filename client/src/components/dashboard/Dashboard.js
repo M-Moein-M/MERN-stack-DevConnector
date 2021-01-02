701509b930ec00e52b3a2850c985ec8530792935
Moein <mohammadmoein.moradi@gmail.com>
2021-01-02 11:13:38 +0330

    chore: add link to create a profile for new users

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,14 +1,34 @@
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { getCurrentProfile } from '../../actions/profile';
 import React, { Fragment, useEffect } from 'react';
 import Spinner from '../layout/Spinner';
 
-const Dashboard = ({ getCurrentProfile, auth, profile: { profile, loading } }) => {
+const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
   useEffect(() => {
     getCurrentProfile();
   }, []);
-  return loading && profile == null ? <Spinner /> : <Fragment>dashboard test</Fragment>;
+  return loading && profile == null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className='large text-primary'>Dashboard</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Welcome {user && user.name}
+      </p>
+      {profile !== null ? (
+        <Fragment>has</Fragment>
+      ) : (
+        <Fragment>
+          <p>You can create your profile right here </p>
+          <Link to='/create-profile' className='btn btn-primary my-1'>
+            Create Profile
+          </Link>
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
 
 Dashboard.prototype = {
