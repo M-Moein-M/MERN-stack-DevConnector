b53071e4e021176106ffa03138f4f395483d33f0
Moein <mohammadmoein.moradi@gmail.com>
2021-01-02 10:33:18 +0330

    chore: redirect from landing page if user's logged in

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,6 +1,13 @@
 import { Fragment } from 'react';
-import { Link } from 'react-router-dom';
-const Landing = () => {
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+
+const Landing = ({ isAuthenticated }) => {
+  if (isAuthenticated) {
+    return <Redirect to='/dashboard' />;
+  }
+
   return (
     <Fragment>
       <section className='landing'>
@@ -25,4 +32,12 @@ const Landing = () => {
   );
 };
 
-export default Landing;
+Landing.propTypes = {
+  isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps)(Landing);
