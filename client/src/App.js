d2d975dd32d08c72e96c8a2d4e05195b9879bb27
Moein <mohammadmoein.moradi@gmail.com>
2021-01-02 16:38:53 +0330

    chore: add CreateProfile component

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
+import CreateProfile from './components/profile-forms/CreateProfile';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -36,6 +37,7 @@ const App = () => {
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute exact path='/create-profile' component={CreateProfile} />
             </Switch>
           </section>
         </Fragment>
