fe2174bfa823e80247d3dac60e8e91eb495ef83a
Moein <mohammadmoein.moradi@gmail.com>
2021-01-03 14:17:55 +0330

    chore: add createProfile action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -15,3 +15,35 @@ export const getCurrentProfile = () => async (dispatch) => {
     });
   }
 };
+
+// create or update a profile
+export const createProfile = (formData, history, edit = false) => async (dispatch) => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const res = await axios.post('/api/profile', formData, config);
+
+    dispatch({ type: GET_PROFILE, payload: res.data });
+    dispatch(setAlert(edit ? 'Profile updated' : 'Profile created'));
+
+    if (!edit) {
+      history.push('/dashboard');
+    }
+  } catch (err) {
+    const errors = err.response.data.errors;
+    console.log(errors);
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
