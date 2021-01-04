af3cbfce8f3ad6dd5ddf727c04522240013f4175
Moein <mohammadmoein.moradi@gmail.com>
2021-01-04 20:03:36 +0330

    feat: add editing profile functionality

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import setAuthToken from './utils/setAuthToken';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
 import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -38,6 +39,7 @@ const App = () => {
               <Route exact path='/login' component={Login} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
               <PrivateRoute exact path='/create-profile' component={CreateProfile} />
+              <PrivateRoute exact path='/edit-profile' component={EditProfile} />
             </Switch>
           </section>
         </Fragment>
--- client/src/components/dashboard/DashboardActions.js
+++ client/src/components/dashboard/DashboardActions.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const DashboardActions = () => {
   return (
-    <div class='dash-buttons'>
+    <div className='dash-buttons'>
       <Link to='/edit-profile' className='btn btn-light'>
         <i className='fas fa-user-circle text-primary'></i> Edit Profile{' '}
       </Link>
