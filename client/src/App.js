34df901b1a7f7b925ec12b7029f388ebf856be57
Moein <mohammadmoein.moradi@gmail.com>
2021-01-05 15:35:08 +0330

    feat: add component for adding new education

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
 import CreateProfile from './components/profile-forms/CreateProfile';
 import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
 import EditProfile from './components/profile-forms/EditProfile';
 
 // Redux
@@ -42,6 +43,7 @@ const App = () => {
               <PrivateRoute exact path='/create-profile' component={CreateProfile} />
               <PrivateRoute exact path='/edit-profile' component={EditProfile} />
               <PrivateRoute exact path='/add-experience' component={AddExperience} />
+              <PrivateRoute exact path='/add-education' component={AddEducation} />
             </Switch>
           </section>
         </Fragment>
