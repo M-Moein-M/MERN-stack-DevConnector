551263ec8fa2e7581aecedb7c2dac9e20d91b6ad
Moein <mohammadmoein.moradi@gmail.com>
2021-01-05 15:22:50 +0330

    feat: add component to add new experience

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import setAuthToken from './utils/setAuthToken';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
 import CreateProfile from './components/profile-forms/CreateProfile';
+import AddExperience from './components/profile-forms/AddExperience';
 import EditProfile from './components/profile-forms/EditProfile';
 
 // Redux
@@ -40,6 +41,7 @@ const App = () => {
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
               <PrivateRoute exact path='/create-profile' component={CreateProfile} />
               <PrivateRoute exact path='/edit-profile' component={EditProfile} />
+              <PrivateRoute exact path='/add-experience' component={AddExperience} />
             </Switch>
           </section>
         </Fragment>
