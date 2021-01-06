7d1aa28ebe50313b0d7efd6755db6fc4ee59855a
Moein <mohammadmoein.moradi@gmail.com>
2021-01-06 15:16:41 +0330

    chore: add deleteEducation action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -104,3 +104,19 @@ export const addEducation = (formData, history) => async (dispatch) => {
     });
   }
 };
+
+// delete experience
+export const deleteExperience = (id) => async (dispatch) => {
+  try {
+    const res = await axios.delete(`/api/profile/experience/${id}`);
+
+    dispatch({ type: UPDATE_PROFILE, payload: res.data });
+
+    dispatch(setAlert('Experience removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
