bb1f4dfd1308f9f0d014a4503d5e6c457d22c628
Moein <mohammadmoein.moradi@gmail.com>
2021-01-07 17:02:43 +0330

    fix: change class to className

--- client/src/components/profile-forms/AddEducation.js
+++ client/src/components/profile-forms/AddEducation.js
@@ -23,18 +23,18 @@ const AddEducation = ({ addEducation, history }) => {
 
   return (
     <Fragment>
-      <h1 class='large text-primary'>Add Your Education</h1>
-      <p class='lead'>
-        <i class='fas fa-code-branch'></i> Add any school/bootcamp that you have attended
+      <h1 className='large text-primary'>Add Your Education</h1>
+      <p className='lead'>
+        <i className='fas fa-code-branch'></i> Add any school/bootcamp that you have attended
       </p>
       <small>* = required field</small>
       <form
-        class='form'
+        className='form'
         onSubmit={(e) => {
           e.preventDefault();
           addEducation(formData, history);
         }}>
-        <div class='form-group'>
+        <div className='form-group'>
           <input
             type='text'
             placeholder='* School or Bootcamp'
@@ -44,7 +44,7 @@ const AddEducation = ({ addEducation, history }) => {
             required
           />
         </div>
-        <div class='form-group'>
+        <div className='form-group'>
           <input
             type='text'
             placeholder='* Degree or Certificate'
@@ -54,7 +54,7 @@ const AddEducation = ({ addEducation, history }) => {
             required
           />
         </div>
-        <div class='form-group'>
+        <div className='form-group'>
           <input
             type='text'
             placeholder='Field of Study'
@@ -63,11 +63,11 @@ const AddEducation = ({ addEducation, history }) => {
             onChange={(e) => onChange(e)}
           />
         </div>
-        <div class='form-group'>
+        <div className='form-group'>
           <h4>From Date</h4>
           <input type='date' name='from' value={from} onChange={(e) => onChange(e)} />
         </div>
-        <div class='form-group'>
+        <div className='form-group'>
           <p>
             <input
               type='checkbox'
@@ -83,7 +83,7 @@ const AddEducation = ({ addEducation, history }) => {
             Current Study
           </p>
         </div>
-        <div class='form-group'>
+        <div className='form-group'>
           <h4>To Date</h4>
           <input
             type='date'
@@ -93,7 +93,7 @@ const AddEducation = ({ addEducation, history }) => {
             disabled={toDateDisabled ? 'disabled' : ''}
           />
         </div>
-        <div class='form-group'>
+        <div className='form-group'>
           <textarea
             name='description'
             cols='30'
@@ -102,8 +102,8 @@ const AddEducation = ({ addEducation, history }) => {
             value={description}
             onChange={(e) => onChange(e)}></textarea>
         </div>
-        <input type='submit' class='btn btn-primary my-1' />
-        <a class='btn btn-light my-1' href='dashboard.html'>
+        <input type='submit' className='btn btn-primary my-1' />
+        <a className='btn btn-light my-1' href='dashboard.html'>
           Go Back
         </a>
       </form>
@@ -112,7 +112,7 @@ const AddEducation = ({ addEducation, history }) => {
 };
 
 AddEducation.propTypes = {
-  AddEducation: PropTypes.func.isRequired,
+  addEducation: PropTypes.func.isRequired,
 };
 
 export default connect(null, { addEducation })(AddEducation);
