ac27d54a2eb9e633bc28d36ee4de3ac8a71821fd
Moein <mohammadmoein.moradi@gmail.com>
2021-01-07 16:59:52 +0330

    feat: add deleting education

--- client/src/components/dashboard/Education.js
+++ client/src/components/dashboard/Education.js
@@ -2,8 +2,9 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Moment from 'react-moment';
+import { deleteEducation } from '../../actions/profile';
 
-const Education = ({ education }) => {
+const Education = ({ education, deleteEducation }) => {
   const educations = education.map((edu) => (
     <tr key={edu._id}>
       <td>{edu.school}</td>
@@ -13,7 +14,9 @@ const Education = ({ education }) => {
         {edu.to === null ? ' Now' : <Moment format='YYYY/MM/DD'>{edu.to}</Moment>}
       </td>
       <td>
-        <button className='btn btn-danger'>Delete</button>
+        <button className='btn btn-danger' onClick={() => deleteEducation(edu._id)}>
+          Delete
+        </button>
       </td>
     </tr>
   ));
@@ -38,6 +41,7 @@ const Education = ({ education }) => {
 
 Education.propTypes = {
   education: PropTypes.array.isRequired,
+  deleteEducation: PropTypes.func.isRequired,
 };
 
-export default Education;
+export default connect(null, { deleteEducation })(Education);
