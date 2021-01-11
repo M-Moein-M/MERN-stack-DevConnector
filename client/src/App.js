fa4cd1848704fcab395dc0df7548ee789cf59ba8
Moein <mohammadmoein.moradi@gmail.com>
2021-01-11 15:13:16 +0330

    chore: add getting profiles on developers tab

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import PrivateRoute from './components/routing/PrivateRoute';
 import CreateProfile from './components/profile-forms/CreateProfile';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
+import Profiles from './components/profiles/Profiles';
 import EditProfile from './components/profile-forms/EditProfile';
 
 // Redux
@@ -39,6 +40,8 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <Route exact path='/profiles' component={Profiles} />
+
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
               <PrivateRoute exact path='/create-profile' component={CreateProfile} />
               <PrivateRoute exact path='/edit-profile' component={EditProfile} />
