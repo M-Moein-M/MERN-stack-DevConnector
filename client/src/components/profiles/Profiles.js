7a681e6439fefbca1c557930d5283acc9accd2f8
Moein <mohammadmoein.moradi@gmail.com>
2021-01-11 15:46:16 +0330

    feat: add showing all the users profiles

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -2,14 +2,37 @@ import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import ProfileItem from './ProfileItem';
 import { getProfiles } from '../../actions/profile';
+import profile from '../../reducers/profile';
 
 const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
   useEffect(() => {
     getProfiles();
   }, []);
 
-  return <div></div>;
+  return (
+    <Fragment>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <h1 className='large text-primary'>Developers</h1>
+          <p className='lead'>
+            <i className='fab fa-connectdevelop'></i> Brows and connect with developers
+          </p>
+
+          <div className='profiles'>
+            {profiles.length > 0 ? (
+              profiles.map((profile) => <ProfileItem key={profile._id} profile={profile} />)
+            ) : (
+              <h4>No Profiles found.</h4>
+            )}
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
 
 Profiles.propTypes = {
