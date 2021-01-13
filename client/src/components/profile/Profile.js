e34b1286d5b1b6ec0546977c9cb4f69139c5fae7
Moein <mohammadmoein.moradi@gmail.com>
2021-01-13 14:45:09 +0330

    feat: add some routing links in user's profile page

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,15 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
+import { Link } from 'react-router-dom';
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
   useEffect(() => {
     getProfileById(match.params.id);
-  }, [getProfileById]);
+  }, [getProfileById, match.params.id]);
 
-  return <div>PROFILE</div>;
+  return (
+    <Fragment>
+      {profile === null || loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <Link to='/profiles' className='btn btn-light'>
+            Back To Profiles
+          </Link>
+          {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (
+            <Link to='/edit-profile' className='btn btn-dark'>
+              Edit Profile
+            </Link>
+          )}
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
 
 Profile.propTypes = {
