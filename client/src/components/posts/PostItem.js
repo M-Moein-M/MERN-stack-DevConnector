b9ff4ca0af191aaed434c3b106a6ba5ef9416c5d
Moein <mohammadmoein.moradi@gmail.com>
2021-01-17 17:36:30 +0330

    feat: add unliking and liking a post

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,8 +3,14 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
+import { addLike, removeLike } from '../../actions/post';
 
-const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments, date } }) => {
+const PostItem = ({
+  addLike,
+  removeLike,
+  auth,
+  post: { _id, text, name, avatar, user, likes, comments, date },
+}) => {
   return (
     <div className='post bg-white p-1 my-1'>
       <div>
@@ -18,11 +24,11 @@ const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments
         <p className='post-date'>
           Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>{' '}
         </p>
-        <button type='button' className='btn btn-light'>
+        <button onClick={(e) => addLike(_id)} type='button' className='btn btn-light'>
           <i className='fas fa-thumbs-up'></i>
           <span> {likes.length > 0 && <span>{likes.length}</span>}</span>
         </button>
-        <button type='button' className='btn btn-light'>
+        <button onClick={(e) => removeLike(_id)} type='button' className='btn btn-light'>
           <i className='fas fa-thumbs-down'></i>
         </button>
         <Link to={`/post/${_id}`} className='btn btn-primary'>
@@ -49,4 +55,4 @@ const mapStateToProps = (state) => ({
   auth: state.auth,
 });
 
-export default connect(mapStateToProps, {})(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike })(PostItem);
