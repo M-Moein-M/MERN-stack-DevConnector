0d868d8db287ffd8682194a1c39d7c9a4773ee44
Moein <mohammadmoein.moradi@gmail.com>
2021-01-17 17:23:52 +0330

    chore: add addLike action

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
 
 // get posts
 export const getPosts = () => async (dispatch) => {
@@ -8,6 +8,20 @@ export const getPosts = () => async (dispatch) => {
     const res = await axios.get('/api/posts');
 
     dispatch({ type: GET_POSTS, payload: res.data });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// add like
+export const addLike = (postId) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/like/${postId}`);
+
+    dispatch({ type: UPDATE_LIKES, payload: { postId, likes: res.data } });
   } catch (err) {
     dispatch({
       type: POST_ERROR,
