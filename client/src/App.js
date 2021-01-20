b168e235f53c2d6c658cce37e6f1ba5706a1e001
Moein <mohammadmoein.moradi@gmail.com>
2021-01-20 13:48:44 +0330

    chore: add discussion link functionality

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
 import EditProfile from './components/profile-forms/EditProfile';
 
 // Redux
@@ -51,6 +52,7 @@ const App = () => {
               <PrivateRoute exact path='/add-experience' component={AddExperience} />
               <PrivateRoute exact path='/add-education' component={AddEducation} />
               <PrivateRoute exact path='/posts' component={Posts} />
+              <PrivateRoute exact path='/posts/:id' component={Post} />
             </Switch>
           </section>
         </Fragment>
