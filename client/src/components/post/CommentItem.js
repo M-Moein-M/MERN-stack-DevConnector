ef0a2b565bbc5b374a62842cfbd1fa2b3b62f951
Moein <mohammadmoein.moradi@gmail.com>
2021-01-23 12:59:44 +0330

    feat: add delete comment functionality

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -3,8 +3,14 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Moment from 'react-moment';
+import { deleteComment } from '../../actions/post';
 
-const CommentItem = ({ postId, comment: { _id, text, name, avatar, user, date }, auth }) => {
+const CommentItem = ({
+  postId,
+  deleteComment,
+  comment: { _id, text, name, avatar, user, date },
+  auth,
+}) => {
   return (
     <div className='post bg-white p-1 my-1'>
       <div>
@@ -18,6 +24,14 @@ const CommentItem = ({ postId, comment: { _id, text, name, avatar, user, date },
         <p className='post-date'>
           Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>{' '}
         </p>
+        {!auth.loading && user === auth.user._id && (
+          <button
+            onClick={(e) => deleteComment(postId, _id)}
+            type='button'
+            className='btn btn-danger'>
+            <i className='fas fa-times'></i>
+          </button>
+        )}
       </div>
     </div>
   );
@@ -27,10 +41,11 @@ CommentItem.propTypes = {
   postId: PropTypes.string.isRequired,
   comment: PropTypes.object.isRequired,
   auth: PropTypes.object.isRequired,
+  deleteComment: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   auth: state.auth,
 });
 
-export default connect(mapStateToProps, {})(CommentItem);
+export default connect(mapStateToProps, { deleteComment })(CommentItem);
