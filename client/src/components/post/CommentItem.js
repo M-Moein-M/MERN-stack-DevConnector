ffed3eccbf014964b9daae4609b07762276d268a
Moein <mohammadmoein.moradi@gmail.com>
2021-01-23 12:52:16 +0330

    chore: add CommentItem component

---
+++ client/src/components/post/CommentItem.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Moment from 'react-moment';
+
+const CommentItem = ({ postId, comment: { _id, text, name, avatar, user, date }, auth }) => {
+  return (
+    <div className='post bg-white p-1 my-1'>
+      <div>
+        <Link to={`/profile/${user}`}>
+          <img className='round-img' src={avatar} alt='' />
+          <h4>{name}</h4>
+        </Link>
+      </div>
+      <div>
+        <p className='my-1'>{text}</p>
+        <p className='post-date'>
+          Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>{' '}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+CommentItem.propTypes = {
+  postId: PropTypes.string.isRequired,
+  comment: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, {})(CommentItem);
